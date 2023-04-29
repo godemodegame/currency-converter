a4a27db8608e84b46b33bf7ce6458abc0142c017
kirill.kirilenko <kirill.kirilenko@latoken.com>
2023-04-29 13:15:58 +0400

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Kirill Kirilenko on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
